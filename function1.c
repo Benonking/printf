1578baf92833735ad1fe2f6f240012d5caa84f29
Benonking <benonkghoma@gmail.com>
2022-10-18 02:33:18 -0700

    Add additional functions

--- function1.c
+++ function1.c
@@ -7,27 +7,25 @@
  */
 int print_unsgined_number(unsigned int n)
 {
-        int div;
-        int len;
-        unsigned int num;
+	int div;
+	int len;
+	unsigned int num;
 
-        div = 1;
-        len = 0;
+	div = 1;
+	len = 0;
+	num = n;
 
-        num = n;
-
-        for (; num / div > 9; )
-                div *= 10;
-
-        for (; div != 0; )
-        {
-                len += _write_char('0' + num / div);
-                num %= div;
-                div /= 10;
-        }
-
-        return (len);
+	for (; num / div > 9; )
+		div *= 10;
+	for (; div != 0; )
+	{
+		len += _write_char('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+	return (len);
 }
+
 /**
  * print_number - prints a number send to this function
  * @args: List of arguments
@@ -35,33 +33,32 @@ int print_unsgined_number(unsigned int n)
  */
 int print_number(va_list args)
 {
-        int n;
-        int div;
-        int len;
-        unsigned int num;
+	int n;
+	int div;
+	int len;
+	unsigned int num;
 
-        n  = va_arg(args, int);
-        div = 1;
-        len = 0;
+	n  = va_arg(args, int);
+	div = 1;
+	len = 0;
 
-        if (n < 0)
-        {
-                len += _write_char('-');
-                num = n * -1;
-        }
-        else
-                num = n;
+	if (n < 0)
+	{
+		len += _write_char('-');
+		num = n * -1;
+	}
+	else
+		num = n;
 
-        for (; num / div > 9; )
-                div *= 10;
+	for (; num / div > 9; )
+		div *= 10;
 
-        for (; div != 0; )
-        {
-                len += _write_char('0' + num / div);
-                num %= div;
-                div /= 10;
-        }
-
-        return (len);
+	for (; div != 0; )
+	{
+		len += _write_char('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+	return (len);
 }
 
